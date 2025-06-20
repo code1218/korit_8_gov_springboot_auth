77243dee76d04e727269ed118ece2e74912914e3
code1218 <skjil1218@gmail.com>
2025-06-20 11:15:07 +0900

    security filter

--- src/main/java/com/korit/authstudy/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/korit/authstudy/security/filter/JwtAuthenticationFilter.java
@@ -1,16 +1,53 @@
 package com.korit.authstudy.security.filter;
 
+import com.korit.authstudy.domain.entity.User;
+import com.korit.authstudy.repository.UsersRepository;
+import com.korit.authstudy.security.jwt.JwtUtil;
+import com.korit.authstudy.security.model.PrincipalUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter implements Filter {
+
+    private final JwtUtil jwtUtil;
+    private final UsersRepository usersRepository;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("JWT AccessToken 검사");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String authorization = request.getHeader("Authorization");
+        System.out.println("Bearer 토큰: " + authorization);
+        if (jwtUtil.isBearer(authorization)) {
+            String accessToken = jwtUtil.removeBearer(authorization);
+            try {
+                Claims claims = jwtUtil.getClaims(accessToken);
+                String id = claims.getId();
+                Integer userId = Integer.parseInt(id);
+                Optional<User> foundUserOptional = usersRepository.findById(userId);
+                foundUserOptional.ifPresentOrElse((user) -> {
+                    // UserEntity를 Security에서 인증객체로 사용할 PrincipalUser로 변환
+                    PrincipalUser principalUser = PrincipalUser.builder()
+
+                            .build();
+                }, () -> {
+                    // 예외처리
+                });
+
+            } catch (JwtException e) {
+                e.printStackTrace();
+            }
+        }
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
